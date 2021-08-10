f800a8a68e77e36e4436d07f10a494875ecc9315
StarunneR <cloudpool2017@outlook.com>
2021-08-11 00:28:15 +0800

    auto

--- commit.sh
+++ commit.sh
@@ -3,5 +3,6 @@ git commit -m "auto" #暂存文件，暂存附加信息为COMMENT
 git push gitlab master #推送到远端名gitlab的master分支
 git push github master #推送到远端名github的master分支
 hexo clean #清除博客本地生成器缓存
-hexo g -d #渲染并推送到远端名为github的gh-page分支
+hexo g #渲染生成静态网页
+hexo d #推送到远端名为github的gh-page分支
 read -n 1 #暂停运行
