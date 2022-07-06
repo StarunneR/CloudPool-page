e521d6a2296959edb61cfaffd6a88946326d703e
StarunneR <cloudpool2017@outlook.com>
2022-07-07 00:35:28 +0800

    auto

--- themes/vexo/source/js/script.js
+++ themes/vexo/source/js/script.js
@@ -7,21 +7,6 @@
   var top = $('.scroll-top')
   var catalog = $('.catalog-container .toc-main')
   var isOpen = false
-  var OriginTitile = document.title;
-  var titleTime;
-
-  document.addEventListener('visibilitychange', function() {
-      if (document.hidden) {
-          document.title = '(つェ⊂)看不见我看不见我~ ' + OriginTitile;
-          clearTimeout(titleTime);
-      }
-      else {
-          document.title = '(*´∇｀*) 被你发现啦~ ' + OriginTitile;
-          titleTime = setTimeout(function() {
-              document.title = OriginTitile;
-          }, 1500);
-      }
-  });
 
   $(document).ready(function () {
     NProgress.start()
