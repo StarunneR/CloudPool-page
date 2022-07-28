6dd2c207b31082b469d6e87a54febd74d31b34d9
StarunneR <cloudpool2017@outlook.com>
2022-07-28 23:09:16 +0800

    test

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 git add . #跟踪所有文件，除.gitignore文件规定的
 read -p "Commit Message: " message #输入保存Commit Message
-git commit -m message #暂存文件，暂存附加信息为COMMENT
+git commit -m "$message" #暂存文件，暂存附加信息为COMMENT
 git push gitlab master #推送到远端名gitlab的master分支
 git push github master #推送到远端名github的master分支
 hexo clean #清除博客本地生成器缓存
