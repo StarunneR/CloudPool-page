5666509d6d30b90782e3e25edc395af55a5ea057
StarunneR <cloudpool2017@outlook.com>
2022-07-28 22:47:57 +0800

    auto

--- commit.sh
+++ commit.sh
@@ -1,5 +1,6 @@
 git add . #跟踪所有文件，除.gitignore文件规定的
-git commit -m "auto" #暂存文件，暂存附加信息为COMMENT
+read -p "Commit Message: " message
+git commit -m message #暂存文件，暂存附加信息为COMMENT
 git push gitlab master #推送到远端名gitlab的master分支
 git push github master #推送到远端名github的master分支
 hexo clean #清除博客本地生成器缓存
