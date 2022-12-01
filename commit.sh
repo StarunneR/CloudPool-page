cae7324d2d029b05c417896122809e53808affb3
StarunneR <cloudpool2017@outlook.com>
2022-12-01 16:16:37 +0800

    quick commit

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
-# git rm -r --cached . # 清除缓存
+git rm -r --cached . # 清除缓存
 git add . #跟踪所有文件，除.gitignore文件规定的
 read -p "Commit Message: " message #输入保存Commit Message为函数message
 git commit -m "$message" #暂存文件，暂存附加信息message函数
