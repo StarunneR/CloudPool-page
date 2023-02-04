5e81ba9dee672b9af2961638c8f9842e53bf64de
StarunneR <cloudpool2017@outlook.com>
2023-02-05 01:31:44 +0800

    quick commit

--- themes/vexo/source/js/script.js
+++ themes/vexo/source/js/script.js
@@ -96,13 +96,14 @@
     }, 300)
   })
 
+  //fold_action.js
   $(document).ready(function () {
     $(document).on('click', '.fold_hider', function () {
-      $('>.fold', this.parentNode).slideToggle();
+      $('>.fold_content', this.parentNode).slideToggle();
       $('>:first', this).toggleClass('open');
     });
     //默认情况下折叠
-    $("div.fold").css("display", "none");
+    $("div.fold_content").css("display", "none");
   })
 
   //tag.js
