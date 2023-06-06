a26f82863202c9f03d637318992bccc87ae7edd3
StarunneR <cloudpool2017@outlook.com>
2023-06-06 22:39:55 +0800

    quick commit

--- themes/vexo/scripts/aplayer_tag.js
+++ themes/vexo/scripts/aplayer_tag.js
@@ -1,6 +1,6 @@
 function aplayer(title, content) {
   return '<link rel="stylesheet" href="/css/APlayer.min.css"><div id="aplayer"></div><script src="/js/APlayer.min.js"></script>' +
-  '<script>const ap = new APlayer({container: document.getElementById("aplayer"),lrcType: 3,loop: "all",audio: [{' +
+  '<script>const ap = new APlayer({container: document.getElementById("aplayer"),lrcType: 3,loop: "no",audio: [{' +
   hexo.render.renderSync({
     text: content,
     engine: ''
