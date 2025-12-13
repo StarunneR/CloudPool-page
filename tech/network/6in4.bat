87b08bdd8d643e7be059bd38f3e0dea89df26ba4
StarunneR <cloudpool2017@outlook.com>
2025-12-14 00:03:34 +0800

    Site updated: 2025-12-14 00:03:28

---
+++ tech/network/6in4.bat
@@ -0,0 +1,126 @@
+@echo off
+echo #4in6 Server Ping Test#
+echo.
+echo North America
+echo.
+echo Ashburn, VA, US
+ping 216.66.22.2 -w 1000 -n 5
+echo.
+echo Calgary, AB, CA
+ping 216.218.200.58 -w 1000 -n 5
+echo.
+echo Chicago, IL, US 
+ping 184.105.253.14 -w 1000 -n 5
+echo.
+echo Dallas, TX, US
+ping 184.105.253.10 -w 1000 -n 5
+echo.
+echo Denver, CO, US
+ping 184.105.250.46 -w 1000 -n 5
+echo.
+echo Fremont, CA, US
+ping 72.52.104.74 -w 1000 -n 5
+echo.
+echo Fremont, CA, US
+ping 64.62.134.130 -w 1000 -n 5
+echo.
+echo Honolulu, HI, US
+ping 64.71.156.86 -w 1000 -n 5
+echo.
+echo Kansas City, MO, US
+ping 216.66.77.230 -w 1000 -n 5
+echo.
+echo Los Angeles, CA, US
+ping 66.220.18.42 -w 1000 -n 5
+echo.
+echo Miami, FL, US
+ping 209.51.161.58 -w 1000 -n 5
+echo.
+echo New York, NY, US
+ping 209.51.161.14 -w 1000 -n 5
+echo.
+echo Phoenix, AZ, US
+ping 66.220.7.82 -w 1000 -n 5
+echo.
+echo Seattle, WA, US
+ping 216.218.226.238 -w 1000 -n 5
+echo.
+echo Toronto, ON, CA
+ping 216.66.38.58 -w 1000 -n 5
+echo.
+echo Winnipeg, MB, CA
+ping 184.105.255.26 -w 1000 -n 5
+echo.
+echo Europe
+echo.
+echo Amsterdam, NL
+ping 216.66.84.46 -w 1000 -n 5
+echo.
+echo Berlin, DE
+ping 216.66.86.114 -w 1000 -n 5
+echo.
+echo Budapest, HU
+ping 216.66.87.14 -w 1000 -n 5
+echo.
+echo Frankfurt, DE
+ping 216.66.80.30 -w 1000 -n 5
+echo.
+echo Lisbon, PT
+ping 216.66.87.102 -w 1000 -n 5
+echo.
+echo London, UK
+ping 216.66.80.26 -w 1000 -n 5
+echo.
+echo London, UK
+ping 216.66.88.98 -w 1000 -n 5
+echo.
+echo Paris, FR
+ping 216.66.84.42 -w 1000 -n 5
+echo.
+echo Prague, CZ
+ping 216.66.86.122 -w 1000 -n 5
+echo.
+echo Stockholm, SE
+ping 216.66.80.90 -w 1000 -n 5
+echo.
+echo Warsaw, PL
+ping 216.66.80.162 -w 1000 -n 5
+echo.
+echo Zurich, CH
+ping 216.66.80.98 -w 1000 -n 5
+echo.
+echo Asia
+echo.
+echo Hong Kong, HK
+ping 216.218.221.6 -w 1000 -n 5
+echo.
+echo Singapore, SG
+ping 216.218.221.42 -w 1000 -n 5
+echo.
+echo Tokyo, JP
+ping 74.82.46.6 -w 1000 -n 5
+echo.
+echo Africa
+echo.
+echo Djibouti City, DJ
+ping 216.66.87.98 -w 1000 -n 5
+echo.
+echo Johannesburg, ZA 
+ping 216.66.87.134 -w 1000 -n 5
+echo.
+echo South America
+echo.
+echo Bogota, CO
+ping 216.66.64.154 -w 1000 -n 5
+echo.
+echo Oceania
+echo.
+echo Sydney, NSW, AU
+ping 216.218.142.50 -w 1000 -n 5
+echo.
+echo Middle East
+echo.
+echo Dubai, AE
+ping 216.66.90.30 -w 1000 -n 5
+echo.
+pause
